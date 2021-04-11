f64efa1eaedb579cd6272e7d33fa018b065daf88
Dan <34944139+danezu1@users.noreply.github.com>
2021-04-11 17:39:21 -0400

    MainMenu Done

--- ProjectFiles/Assets/MainMenu.cs
+++ ProjectFiles/Assets/MainMenu.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public Text difficulty_text;
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -15,6 +19,31 @@ public class MainMenu : MonoBehaviour
     public void FuckU()
     {
         print("FUCK U");
+    }
+    public void changeDifficulty ()
+    {
+
+        string text = difficulty_text.text;
+
+        switch (text)
+        {
+            case "Easy":
+                difficulty_text.text = "Medium";
+                break;
+
+            case "Medium":
+                difficulty_text.text = "Hard";
+                break;
+
+            case "Hard":
+                difficulty_text.text = "Easy";
+                break;
+
+            default: print("Idiot in the case switch, change difficulty");
+                break;
+
+        }
+        
     }
 
     public void QuitGame()
