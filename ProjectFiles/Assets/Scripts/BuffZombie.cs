3f4337e36d8b94152ce56c497065b55e2deb38d3
Clark <t.clark.dumoulin@gnail.com>
2021-04-22 21:43:19 -0400

    made text above zombies bigger

--- ProjectFiles/Assets/Scripts/BuffZombie.cs
+++ ProjectFiles/Assets/Scripts/BuffZombie.cs
@@ -6,7 +6,7 @@ public class BuffZombie : Enemies
     {
         type = WaveManager.ZombieType.Basic;
         targetHeadOffset = new Vector3(0, 1.5f, 0);
-        targetTextOffset = new Vector3(0.5f, 5, 0);
+        targetTextOffset = new Vector3(2f, 5, 0);
         health = 1;
         speed = 1;
         head = transform.GetChild(0).GetChild(0);
